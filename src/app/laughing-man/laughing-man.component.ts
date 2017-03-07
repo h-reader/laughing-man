943cb6e43bf585f9804e4be692e25be62cb27c2b
h-reader <super.black.jack777@gmail.com>
2017-03-07 16:55:15 +0900

    inputからファイルを読み込んでキャンバスに画像を表示

--- src/app/laughing-man/laughing-man.component.ts
+++ src/app/laughing-man/laughing-man.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+const Tracker = require('clmtrackr/clmtrackr.js');
+const faceModel = require('clmtrackr/models/model_pca_20_svm.js');
 
 @Component({
   selector: 'app-laughing-man',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaughingManComponent implements OnInit {
 
+  /** 画像選択後に表示するキャンバス */
+  @ViewChild('dropCanvas') dropCanvas;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * inputで選択したファイルをcanvas上に表示する。
+   * @param inputFile 画像ファイルを選択したinput 
+   */
+  public showImage4Canvas(inputFile: HTMLInputElement) {
+    if(inputFile != null && inputFile.files != null && inputFile.files.length > 0) {
+      this.showCanvas(this.dropCanvas.nativeElement, inputFile.files[0]);
+    }
+  }
+
+  /**
+   * 引数のキャンバスに画像イメージを表示する。
+   * @param canvas 表示対象のキャンバス
+   * @param blob 画像イメージ
+   */
+  private showCanvas(canvas : HTMLCanvasElement, blob: Blob) {
+    // ファイルを読込み
+    var ctx = canvas.getContext('2d');
+    var reader = new FileReader();
+    var image = new Image();
+    reader.onload = (function(theFile) {
+      image.onload = function() {
+        ctx.drawImage(image, 0, 0);
+      }
+      image.src = this.result;
+    });
+    reader.readAsDataURL(blob);
+  }
 }
