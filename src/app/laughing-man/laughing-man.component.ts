5684d1604e622227026328f0c7d7f947c8fb59b9
h-reader <super.black.jack777@gmail.com>
2017-03-28 18:40:17 +0900

    lintエラーを解消

--- src/app/laughing-man/laughing-man.component.ts
+++ src/app/laughing-man/laughing-man.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 require('tracking');
-require('tracking/build/data/face-min')
+require('tracking/build/data/face-min');
 
 @Component({
   selector: 'app-laughing-man',
   templateUrl: './laughing-man.component.html',
   styleUrls: ['./laughing-man.component.scss']
 })
+
+
 export class LaughingManComponent implements OnInit {
 
   /** 画像選択後に表示するキャンバス */
@@ -20,10 +22,10 @@ export class LaughingManComponent implements OnInit {
 
   /**
    * inputで選択したファイルをcanvas上に表示する。
-   * @param inputFile 画像ファイルを選択したinput 
+   * @param inputFile 画像ファイルを選択したinput
    */
   public showImage4Canvas(inputFile: HTMLInputElement) {
-    if(inputFile != null && inputFile.files != null && inputFile.files.length > 0) {
+    if (inputFile != null && inputFile.files != null && inputFile.files.length > 0) {
       this.showCanvas(this.dropCanvas.nativeElement, inputFile.files[0]);
     }
   }
@@ -33,15 +35,15 @@ export class LaughingManComponent implements OnInit {
    * @param canvas 表示対象のキャンバス
    * @param blob 画像イメージ
    */
-  private showCanvas(canvas : HTMLCanvasElement, blob: Blob) {
+  private showCanvas(canvas: HTMLCanvasElement, blob: Blob) {
     // ファイルを読込み
-    var ctx = canvas.getContext('2d');
-    var reader = new FileReader();
-    var image = new Image();
+    const ctx = canvas.getContext('2d');
+    const reader = new FileReader();
+    const image = new Image();
     reader.onload = (function(theFile) {
       image.onload = function() {
         ctx.drawImage(image, 0, 0);
-      }
+      };
       image.src = this.result;
     });
     reader.readAsDataURL(blob);
@@ -49,7 +51,7 @@ export class LaughingManComponent implements OnInit {
 
   public convertImage() {
 
-    let tracker = new tracking.ObjectTracker(['face']);
+    const tracker = new tracking.ObjectTracker(['face']);
 
     tracker.on('track', (event) => {
       if (event.data.length === 0) {
@@ -64,6 +66,5 @@ export class LaughingManComponent implements OnInit {
 
     tracking.track('#dropCanvas', tracker);
   }
-  
-  
+
 }
